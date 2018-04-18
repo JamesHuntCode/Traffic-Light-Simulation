97ddb2cc81fe8eaaa4ff646e9f460caa73214e80
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-04-18 11:40:58 +0100

    new car class added

---
+++ TrafficLightClient/TrafficLightClient/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TrafficLightClient
+{
+    public class Car
+    {
+        public float PosX { get; set; }
+        public float PosY { get; set; }
+        public Color Col { get; set; }
+
+        public Car(float x, float y, string c)
+        {
+            this.PosX = x;
+            this.PosY = y;
+            this.Col = ColorTranslator.FromHtml(c);
+        }
+    }
+}
--- TrafficLightClient/TrafficLightClient/frmMain.Designer.cs
+++ TrafficLightClient/TrafficLightClient/frmMain.Designer.cs
@@ -32,6 +32,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.pnlControlsBG = new System.Windows.Forms.Panel();
             this.pnlControls = new System.Windows.Forms.Panel();
+            this.pbarConnecting = new System.Windows.Forms.ProgressBar();
             this.grpFunctionality = new System.Windows.Forms.GroupBox();
             this.btnAddCar = new System.Windows.Forms.Button();
             this.btnConnect = new System.Windows.Forms.Button();
@@ -48,7 +49,6 @@
             this.lblCopyright = new System.Windows.Forms.Label();
             this.lblVersion = new System.Windows.Forms.Label();
             this.radAutoConnect = new System.Windows.Forms.CheckBox();
-            this.pbarConnecting = new System.Windows.Forms.ProgressBar();
             this.tmrConnecting = new System.Windows.Forms.Timer(this.components);
             this.pnlControlsBG.SuspendLayout();
             this.pnlControls.SuspendLayout();
@@ -61,9 +61,10 @@
             // pnlControlsBG
             // 
             this.pnlControlsBG.Controls.Add(this.pnlControls);
-            this.pnlControlsBG.Location = new System.Drawing.Point(849, 12);
+            this.pnlControlsBG.Location = new System.Drawing.Point(1132, 15);
+            this.pnlControlsBG.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlControlsBG.Name = "pnlControlsBG";
-            this.pnlControlsBG.Size = new System.Drawing.Size(285, 700);
+            this.pnlControlsBG.Size = new System.Drawing.Size(380, 862);
             this.pnlControlsBG.TabIndex = 0;
             // 
             // pnlControls
@@ -77,26 +78,38 @@
             this.pnlControls.Controls.Add(this.lstServerEcho);
             this.pnlControls.Controls.Add(this.pictureBox1);
             this.pnlControls.Controls.Add(this.label1);
-            this.pnlControls.Location = new System.Drawing.Point(3, 3);
+            this.pnlControls.Location = new System.Drawing.Point(4, 4);
+            this.pnlControls.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlControls.Name = "pnlControls";
-            this.pnlControls.Size = new System.Drawing.Size(279, 694);
+            this.pnlControls.Size = new System.Drawing.Size(372, 854);
             this.pnlControls.TabIndex = 1;
+            // 
+            // pbarConnecting
+            // 
+            this.pbarConnecting.Location = new System.Drawing.Point(4, 176);
+            this.pbarConnecting.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pbarConnecting.Name = "pbarConnecting";
+            this.pbarConnecting.Size = new System.Drawing.Size(364, 12);
+            this.pbarConnecting.TabIndex = 14;
             // 
             // grpFunctionality
             // 
             this.grpFunctionality.Controls.Add(this.btnAddCar);
-            this.grpFunctionality.Location = new System.Drawing.Point(4, 159);
+            this.grpFunctionality.Location = new System.Drawing.Point(5, 196);
+            this.grpFunctionality.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.grpFunctionality.Name = "grpFunctionality";
-            this.grpFunctionality.Size = new System.Drawing.Size(272, 332);
+            this.grpFunctionality.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpFunctionality.Size = new System.Drawing.Size(363, 409);
             this.grpFunctionality.TabIndex = 11;
             this.grpFunctionality.TabStop = false;
             // 
             // btnAddCar
             // 
-            this.btnAddCar.Font = new System.Drawing.Font("DINPro-Regular", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddCar.Location = new System.Drawing.Point(6, 19);
+            this.btnAddCar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddCar.Location = new System.Drawing.Point(8, 23);
+            this.btnAddCar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAddCar.Name = "btnAddCar";
-            this.btnAddCar.Size = new System.Drawing.Size(260, 52);
+            this.btnAddCar.Size = new System.Drawing.Size(347, 64);
             this.btnAddCar.TabIndex = 12;
             this.btnAddCar.Text = "Add New Car";
             this.btnAddCar.UseVisualStyleBackColor = true;
@@ -104,10 +117,11 @@
             // 
             // btnConnect
             // 
-            this.btnConnect.Font = new System.Drawing.Font("DINPro-Regular", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnConnect.Location = new System.Drawing.Point(3, 84);
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(4, 103);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(273, 52);
+            this.btnConnect.Size = new System.Drawing.Size(364, 64);
             this.btnConnect.TabIndex = 6;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -116,49 +130,54 @@
             // lblServerState
             // 
             this.lblServerState.AutoSize = true;
-            this.lblServerState.Font = new System.Drawing.Font("DINPro-Regular", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblServerState.Location = new System.Drawing.Point(138, 661);
+            this.lblServerState.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblServerState.Location = new System.Drawing.Point(184, 814);
+            this.lblServerState.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblServerState.Name = "lblServerState";
-            this.lblServerState.Size = new System.Drawing.Size(121, 24);
+            this.lblServerState.Size = new System.Drawing.Size(146, 29);
             this.lblServerState.TabIndex = 10;
             this.lblServerState.Text = "current-state";
             // 
             // lblServerStatus
             // 
             this.lblServerStatus.AutoSize = true;
-            this.lblServerStatus.Font = new System.Drawing.Font("DINPro-Regular", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblServerStatus.Location = new System.Drawing.Point(3, 661);
+            this.lblServerStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblServerStatus.Location = new System.Drawing.Point(4, 814);
+            this.lblServerStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblServerStatus.Name = "lblServerStatus";
-            this.lblServerStatus.Size = new System.Drawing.Size(129, 24);
+            this.lblServerStatus.Size = new System.Drawing.Size(162, 29);
             this.lblServerStatus.TabIndex = 9;
             this.lblServerStatus.Text = "Server Status:";
             // 
             // lblServerEcho
             // 
             this.lblServerEcho.AutoSize = true;
-            this.lblServerEcho.Font = new System.Drawing.Font("DINPro-Regular", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblServerEcho.Location = new System.Drawing.Point(3, 494);
+            this.lblServerEcho.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblServerEcho.Location = new System.Drawing.Point(4, 608);
+            this.lblServerEcho.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblServerEcho.Name = "lblServerEcho";
-            this.lblServerEcho.Size = new System.Drawing.Size(219, 27);
+            this.lblServerEcho.Size = new System.Drawing.Size(291, 31);
             this.lblServerEcho.TabIndex = 8;
             this.lblServerEcho.Text = "Recent Server Activity:";
             // 
             // lstServerEcho
             // 
-            this.lstServerEcho.Font = new System.Drawing.Font("DINPro-Regular", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstServerEcho.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstServerEcho.FormattingEnabled = true;
-            this.lstServerEcho.ItemHeight = 17;
-            this.lstServerEcho.Location = new System.Drawing.Point(3, 524);
+            this.lstServerEcho.ItemHeight = 20;
+            this.lstServerEcho.Location = new System.Drawing.Point(4, 645);
+            this.lstServerEcho.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstServerEcho.Name = "lstServerEcho";
-            this.lstServerEcho.Size = new System.Drawing.Size(273, 123);
+            this.lstServerEcho.Size = new System.Drawing.Size(363, 144);
             this.lstServerEcho.TabIndex = 7;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(28, 16);
+            this.pictureBox1.Location = new System.Drawing.Point(37, 20);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(37, 35);
+            this.pictureBox1.Size = new System.Drawing.Size(49, 43);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
@@ -166,34 +185,38 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("DINPro-Regular", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(71, 16);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(95, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(178, 35);
+            this.label1.Size = new System.Drawing.Size(225, 39);
             this.label1.TabIndex = 6;
             this.label1.Text = "User Controls";
             // 
             // pnlSimulationBG
             // 
             this.pnlSimulationBG.Controls.Add(this.pnlSimulation);
-            this.pnlSimulationBG.Location = new System.Drawing.Point(12, 99);
+            this.pnlSimulationBG.Location = new System.Drawing.Point(16, 122);
+            this.pnlSimulationBG.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlSimulationBG.Name = "pnlSimulationBG";
-            this.pnlSimulationBG.Size = new System.Drawing.Size(831, 613);
+            this.pnlSimulationBG.Size = new System.Drawing.Size(1108, 754);
             this.pnlSimulationBG.TabIndex = 1;
             // 
             // pnlSimulation
             // 
-            this.pnlSimulation.Location = new System.Drawing.Point(3, 3);
+            this.pnlSimulation.Location = new System.Drawing.Point(4, 4);
+            this.pnlSimulation.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlSimulation.Name = "pnlSimulation";
-            this.pnlSimulation.Size = new System.Drawing.Size(825, 607);
+            this.pnlSimulation.Size = new System.Drawing.Size(1100, 747);
             this.pnlSimulation.TabIndex = 2;
             // 
             // picAppLogo
             // 
             this.picAppLogo.Image = ((System.Drawing.Image)(resources.GetObject("picAppLogo.Image")));
-            this.picAppLogo.Location = new System.Drawing.Point(12, 12);
+            this.picAppLogo.Location = new System.Drawing.Point(16, 15);
+            this.picAppLogo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.picAppLogo.Name = "picAppLogo";
-            this.picAppLogo.Size = new System.Drawing.Size(88, 81);
+            this.picAppLogo.Size = new System.Drawing.Size(117, 100);
             this.picAppLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picAppLogo.TabIndex = 2;
             this.picAppLogo.TabStop = false;
@@ -201,51 +224,48 @@
             // lblMainHeading
             // 
             this.lblMainHeading.AutoSize = true;
-            this.lblMainHeading.Font = new System.Drawing.Font("DINPro-Regular", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMainHeading.Location = new System.Drawing.Point(106, 31);
+            this.lblMainHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMainHeading.Location = new System.Drawing.Point(141, 38);
+            this.lblMainHeading.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMainHeading.Name = "lblMainHeading";
-            this.lblMainHeading.Size = new System.Drawing.Size(294, 35);
+            this.lblMainHeading.Size = new System.Drawing.Size(363, 39);
             this.lblMainHeading.TabIndex = 3;
             this.lblMainHeading.Text = "Traffic Light Simulation";
             // 
             // lblCopyright
             // 
             this.lblCopyright.AutoSize = true;
-            this.lblCopyright.Font = new System.Drawing.Font("DINPro-Regular", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCopyright.Location = new System.Drawing.Point(12, 715);
+            this.lblCopyright.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright.Location = new System.Drawing.Point(16, 880);
+            this.lblCopyright.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCopyright.Name = "lblCopyright";
-            this.lblCopyright.Size = new System.Drawing.Size(340, 17);
+            this.lblCopyright.Size = new System.Drawing.Size(451, 20);
             this.lblCopyright.TabIndex = 4;
             this.lblCopyright.Text = "©2018 James Hunt and Kyle Rusby Some Rights Reserved";
             // 
             // lblVersion
             // 
             this.lblVersion.AutoSize = true;
-            this.lblVersion.Font = new System.Drawing.Font("DINPro-Regular", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblVersion.Location = new System.Drawing.Point(1049, 715);
+            this.lblVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVersion.Location = new System.Drawing.Point(1399, 880);
+            this.lblVersion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(85, 17);
+            this.lblVersion.Size = new System.Drawing.Size(111, 20);
             this.lblVersion.TabIndex = 5;
             this.lblVersion.Text = "Version: 1.0.0";
             // 
             // radAutoConnect
             // 
             this.radAutoConnect.AutoSize = true;
-            this.radAutoConnect.Font = new System.Drawing.Font("DINPro-Regular", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radAutoConnect.Location = new System.Drawing.Point(661, 69);
+            this.radAutoConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radAutoConnect.Location = new System.Drawing.Point(881, 85);
+            this.radAutoConnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radAutoConnect.Name = "radAutoConnect";
-            this.radAutoConnect.Size = new System.Drawing.Size(185, 24);
+            this.radAutoConnect.Size = new System.Drawing.Size(229, 29);
             this.radAutoConnect.TabIndex = 13;
             this.radAutoConnect.Text = "Connect Automatically";
             this.radAutoConnect.UseVisualStyleBackColor = true;
             this.radAutoConnect.CheckedChanged += new System.EventHandler(this.radAutoConnect_CheckedChanged);
-            // 
-            // pbarConnecting
-            // 
-            this.pbarConnecting.Location = new System.Drawing.Point(3, 143);
-            this.pbarConnecting.Name = "pbarConnecting";
-            this.pbarConnecting.Size = new System.Drawing.Size(273, 10);
-            this.pbarConnecting.TabIndex = 14;
             // 
             // tmrConnecting
             // 
@@ -253,9 +273,9 @@
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1146, 738);
+            this.ClientSize = new System.Drawing.Size(1528, 908);
             this.Controls.Add(this.radAutoConnect);
             this.Controls.Add(this.lblVersion);
             this.Controls.Add(this.lblCopyright);
@@ -265,6 +285,7 @@
             this.Controls.Add(this.pnlControlsBG);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "frmMain";
             this.Text = "Traffic Light Simulation - Client Application";
